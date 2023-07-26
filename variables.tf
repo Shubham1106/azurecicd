bce31eeaec1f42027ea98362169d555be099008d
Shubham1106 <59809984+Shubham1106@users.noreply.github.com>
2023-07-26 11:32:58 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,21 +16,17 @@ variable "instanceCount" {
 }
 
 variable "client_id" {
-    description =   "Client ID (APP ID) of the application"
-    type        =   string
+    defualt = "f02545c4-5306-4d5d-95c2-32155a217d57"
 }
 
 variable "client_secret" {
-    description =   "Client Secret (Password) of the application"
-    type        =   string
+    default = "seX8Q~6gRh67QTCBJ6138RQ6HawBY60nZmxN~bIK"
 }
 
 variable "subscription_id" {
-    description =   "Subscription ID"
-    type        =   string
+    default = "ce9ae6de-058b-4cf9-ac15-d9a6211f2493"
 }
 
 variable "tenant_id" {
-    description =   "Tenant ID"
-    type        =   string
+    default = "7cec1586-3fc7-4379-bbda-fbd23fc9a3c2"
 }
